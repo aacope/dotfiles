80fa37e8f47984a33e8288702636bae621eae6b0
aacope <aaronccopeland@gmail.com>
2016-08-12 07:41:49 -0400

    updated sketchy non-error checking and really long bash script for gentoo install that probably doesn't work

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -1,10 +1,17 @@
-# Next line is everything installed during installation process
+# Name: gentoo_install.sh
+# Description: Kind of maybe sort of broken but not script to automate Gentoo install and really it's use at your own risk at this point.
+# Author: Aaron Copeland
+# Date: 08/12/16
+
+
+
+# Next line is everything installed during installation process, this is a note for later
 #emerge vim sys-kernel/gentoo-sources sys-apps/pciutils syslog-ng cronie mlocate sshd e2fsprogs linux-firmware btrfs-progs grub:2 dhcpcd sudo htop 
 
-# ROOT INSTALLED after install: 
+# ROOT INSTALLED after install: , this is a note for later
 #emerge xf86-video-intel alsa-driver libvirt sys-apps/usermode-utilities app-text/tree 
 
-# USER INSTALLED after install (so far):
+# USER INSTALLED after install (so far): , this is a note for later
 #emerge firefox pulseaudio pavucontrol lm_sensors acpitool xfce4-battery-plugin xfwm4-themes xfce4-sensors-plugin xfce4-mount-plugin firefox-bin virt-manager gnustep-apps/preview qlipper parcellite feh net-irc/irrsi ristretto p7zip dev-vcs/git cifs-utils apvlv evince openoffice-bin app-emulation/docker gedit vlc audacious geany nfs-utils aufs-util btrfs-utils btrfs-progs xrandr arandr pv thunar-shares-plugin gvfsd-fuse sys-fs/fuse bridge-utils app-emulation/spice virtualbox-guest-additions virtualbox-bin grsync leafpad wicd tmux transmission chromium-bin qemu xarchiver meld porthole mesa-progs xarg media-fonts/arphicfonts media-fonts/bitstream-cyberbit media-fonts/droid media-fonts/ipamonafont media-fonts/ja-ipafonts media-fonts/takao-fonts media-fonts/wqy-microhei media-fonts/wqy-zenhei nmap wireshark tcpdump mutt filezilla gimp thunderbird mplayer openconnect libstoken unrar xhost virtualbox-modules rdesktop fortune-mod-homer fortune-mod-hitchhiker fortune-mod-starwars fortune-mod-familyguy cowsay fortune-mod-futurama bsod host bind-tools icedtea www-client/midori 
 
 
@@ -24,12 +31,15 @@ chroot /mnt/gentoo /bin/bash
 source /etc/profile
 export PS1="(chroot) $PS1"
 emerge-webrsync
-eselect profile list
-echo "You should select a profile here if you want a desktop."
+# Kind of sketchy way to select the default desktop profile, use "select profile list" to change (well might be too late if you ran this script haha)
+# Really REALLY need to make this better
+eselect profile list | grep desktop | grep -v 'gnome\|kde\|plasma' | grep -o '[0-9]\+' | grep -v '64\|13\|0' | xargs -n1 eselect profile set 
+
 
 # Starting after modifying make.conf, mirrors, etc and updating system first time
 # below is same as emerge --ask --update --deep --newuse @world
 emerge -auDN @world
+# Next step is optional but wayyyyyy better than nano
 emerge vim
 # Shows current USE variables
 emerge --info | grep ^USE
@@ -39,15 +49,15 @@ emerge --config sys-libs/timezone-data
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-eselect locale list 
-echo "You should select en_US.utf8 if it is available."
+# Kind of sketchy way to automate "eselect locale list" for en_US.utf8...
+eselect locale list | grep en_US.utf8 | grep -o '[0-7]\+' | xargs -n1 eselect locale set
 env-update && source /etc/profile && export PS1="(chroot) $PS1"
 
 # Now onto Configuring the Linux kernel https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Kernel
 emerge --ask sys-kernel/gentoo-sources
 ls -l /usr/src/linux
 emerge --ask sys-apps/pciutils
-# For manual config do this
+# For manual config do this, couldn't automate this, sorry guys!
 #cd /usr/src/linux
 #make menuconfig
 #make && make modules_install && make install
@@ -58,26 +68,31 @@ emerge --ask sys-apps/pciutils
 
 # Genkernel
 emerge --ask sys-kernel/genkernel
-echo Modify the /etc/fstab file with partition layout.
+echo "Modify the /etc/fstab file with partition layout."
 genkernel --install initramfs
 genkernel all
 ls /boot/kernel* /boot/initramfs*
-#Checking for modules find /lib/modules/<kernel version>/ -type f -iname '*.o' -or -iname '*.ko' | less
-# add them to "/etc/conf.d/modules
+# Checking for modules 
+find /lib/modules/<kernel version>/ -type f -iname '*.o' -or -iname '*.ko' | less
+echo "CAUTION: Add the corresponding modules that apply for your machine to /etc/conf.d/modules or it may not boot!"
+# Add the corresponding modules you want to "/etc/conf.d/modules" in this format:
 #modules="3c59x"
 emerge --ask sys-kernel/linux-firmware
 
-echo Change hostname in /etc/conf.d/hostname
-echo Change dns_domain_lo=\"homenetwork\"
+# Change hostname in /etc/conf.d/hostname
+echo "bestmachinever" > /etc/conf.d/hostname
+# Change dns_domain_lo="homenetwork"
+echo "dns_domain_lo=\"network\"" >> /etc/conf.d/net
 emerge --ask --noreplace net-misc/netifrc
-echo config_enp2s0="dhcp" >> /etc/conf.d/net
+echo config_$NETINT="dhcp" >> /etc/conf.d/net
 ln -s /etc/init.d/net.lo /etc/init.d/net.enp2s0
-rc-update add net.enp2s0 default
+rc-update add net.$NETINT default
 echo "put hostname in /etc/hosts"
-echo "set root passwd" #passwd
-emerge syslog-ng
-emerge cronie
-emerge mlocate
+# Better change your passwd
+echo "set root passwd" 
+# Super insecure way of setting passwords for root
+echo "changeme" | passwd root --stdin
+emerge syslog-ng cronie mlocate
 
 rc-update add syslog-ng default
 rc-update add cronie default
@@ -98,7 +113,8 @@ grub2-install /dev/sda
 #grub2-install --target=x86_64-efi --efi-directory=/boot
 grub2-mkconfig -o /boot/grub/grub.cfg
 useradd -m -G users,wheel -s /bin/bash aaron
-passwd aaron
+# Here's that super insecure password setting again! Ewwww
+echo "changeme" | passwd aaron --stdin
 rm /stage3-*.tar.bz2*
 emerge sudo
 exit
